9b828d59bf47ca04e6c94253d6bb904c3064cf13
mhammed <mohamed.assaad.10@gmail.com>
2020-09-25 12:30:14 +0100

    my first portfolio

---
+++ jokes.js
@@ -0,0 +1,5 @@
+function joking() {
+    let jokes = ['i wasnt hoping this joke would display','your mama is so fat,when she wears high heels she strikes oil','Agrima the all father','what do u call a white man with a big dick? micheal jackson','what is the worst part about eating bald pussy? putting the diapers back on(just a joke)'];
+    var i=Math.floor(Math.random()*6);
+    alert(jokes [i])
+}
